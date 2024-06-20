0d887312914ed736a6a253328de812162df9afcc
panos <ploykas@gmail.com>
2024-06-20 20:10:27 +0300

    Update typed.jsx

--- src/typed.jsx
+++ src/typed.jsx
@@ -1,17 +1,15 @@
-import React, { useState, useCallback,useEffect,Children } from "react";
+import React, { useState, useEffect } from "react";
 
+//function typing text
 export function Typed({msg,className}){
     const [text,setText]=useState("");
-   //console.log(msg);
-
     useEffect(  ()=>{
         if (text.length<msg.length){
+            //delay 90 ms 
+            //typing Character every 90 msec
             setTimeout(() => {  setText(text+msg[text.length]); }, 90);
-            console.log(text);
-            
-        }
-        //setText(msg);
-
+            console.log(text);            
+        }       
     },[text]);
     return(
         <p className={className}>{text}</p>
